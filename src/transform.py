2095ff546dd5434f03f8b153dcccb2072f7f094a
Ansgar Prahm <ansgar@prahm.de>
2026-07-05 02:14:52 +0200

    read_html function to transform html into a python object structure

---
+++ src/transform.py
@@ -0,0 +1,28 @@
+import regex
+from lxml import etree
+from pprint import pprint
+
+def read_htmltable(s: str) -> list:
+    table = etree.HTML(s).find("body/table")
+    rows = iter(table)
+    headers = [col.text for col in next(rows)]
+    out = []
+    for row in rows:
+        values = [col.text for col in row]
+        out.append(dict(zip(headers, values)))
+    return out
+
+def read_html(file: str) -> dict:
+    with open(file, 'r') as f:
+        raw_text = f.read()
+    segments = raw_text.split("<h3>")
+    out = {}
+    for segment in segments:
+        if not (name := regex.findall("(?<=<a name='f_..'>..</a> . ).+(?=</h3>)", segment)):
+            continue
+        table_list = regex.findall("<table.+?</table>", segment, flags=regex.DOTALL)
+        out[name[0]] = read_htmltable(table_list[0]), read_htmltable(table_list[1])
+    return out
+
+if __name__ == "__main__":
+    pprint(read_html("test/getdata.html"))
